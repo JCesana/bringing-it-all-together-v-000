a5e7bc99d914823fa52033547f418d827aa1c46b
Josh Cesana <joshuacesana@gmail.com>
2018-07-10 20:20:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -59,6 +59,10 @@ class Dog
     dog = Dog.new(id: row[0], name: row[1], breed: row[2])
   end 
   
+  def self.find_or_create_by
+    
+  end 
+  
   def self.new_from_db(row)
     dog = Dog.new(id: row[0], name: row[1], breed: row[2])
   end 
